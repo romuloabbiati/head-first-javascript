13b84d9d9bec3447676f9164789d98886b494e08
romuloabbiati <rhabbiati@hotmail.com>
2025-07-11 04:56:12 +0100

    parseGuess function and test drive

--- ch8/battleship/battleship.js
+++ ch8/battleship/battleship.js
@@ -63,21 +63,21 @@ var model = {
   }
 };
 
-model.fire("53");
+// model.fire("53");
 
-model.fire("06");
-model.fire("16");
-model.fire("26");
+// model.fire("06");
+// model.fire("16");
+// model.fire("26");
 
-model.fire("34");
-model.fire("24");
-model.fire("44");
+// model.fire("34");
+// model.fire("24");
+// model.fire("44");
 
-model.fire("12");
-model.fire("11");
-model.fire("10");
+// model.fire("12");
+// model.fire("11");
+// model.fire("10");
 
-model.fire("00");
+// model.fire("00");
 
 // CONTROLLER OBJECT
 var controller = {
@@ -86,3 +86,31 @@ var controller = {
     // more code will go here
   }
 };
+
+function parseGuess(guess) {
+  var alphabet = ["A", "B", "C", "D", "E", "F", "G"];
+
+  if (guess === null || guess.length !== 2) {
+    alert("Oops, please enter a letter and a number on the board.");
+  } else {
+    firstChar = guess.charAt(0);
+    var row = alphabet.indexOf(firstChar);
+    var column = guess.charAt(1);
+
+    if (isNaN(row) || isNaN(column)) {
+      alert("Oops, that isn't on the board.");
+    } else if (row < 0 || row >= model.boardSize || 
+                column < 0 || column >= model.boardSize) {
+      alert("Oops, that's off the board!");
+    } else {
+      return row + column;
+    }
+  }
+  return null;
+}
+
+console.log(parseGuess("A0"));
+console.log(parseGuess("B6"));
+console.log(parseGuess("G3"));
+console.log(parseGuess("H0"));
+console.log(parseGuess("A7"));
